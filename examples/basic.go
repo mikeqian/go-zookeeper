93ec81dd7af6ecf77c6fb47c09cd3eba894bd85e
钱露晗 <156595517@qq.com>
2015-09-29 13:03:51 +0800

    clear

--- examples/basic.go
+++ examples/basic.go
@@ -1,49 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"github.com/samuel/go-zookeeper/zk"
+	"github.com/mikeqian/go-zookeeper/zk"
 	"time"
 )
 
 func main() {
+	c1 := register("http://www.baidu.com/v1")
+	defer c1.Close()
+
+	c2 := register("http://www.baidu.com/v2")
+	defer c2.Close()
+
+	for {
+		time.Sleep(time.Second * 30)
+	}
+}
+
+func register(url string) *zk.Conn {
 	c, _, err := zk.Connect([]string{"10.0.2.214:2181", "10.0.2.215:2181", "10.0.2.216:2181"}, time.Second*10)
 	if err != nil {
 		panic(err)
 	}
-
-	defer c.Close()
 	acls := zk.WorldACL(zk.PermAll)
+	s := zk.NewService(c, "/services/log", acls)
+	s.Register(url)
 
-	l := zk.NewLock(c, "/test", acls)
-	if err := l.Lock(); err != nil {
-		fmt.Println(err)
-	}
-
-	val := make(chan int, 3)
-	l2 := zk.NewLock(c, "/test", acls)
-	go func() {
-		if err := l2.Lock(); err != nil {
-			fmt.Println(err)
-		}
-		val <- 2
-		if err := l2.Unlock(); err != nil {
-			fmt.Println(err)
-		}
-		val <- 3
-	}()
-	time.Sleep(time.Millisecond * 100)
-	val <- 1
-	if err := l.Unlock(); err != nil {
-		fmt.Println(err)
-	}
-	if x := <-val; x != 1 {
-		fmt.Printf("Expected 1 instead of %d\n", x)
-	}
-	if x := <-val; x != 2 {
-		fmt.Printf("Expected 2 instead of %d\n", x)
-	}
-	if x := <-val; x != 3 {
-		fmt.Printf("Expected 3 instead of %d\n", x)
-	}
+	return c
 }
